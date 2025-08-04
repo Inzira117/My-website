bee8cb7e91af7b699f47a5a8db3dd47a97f4cd9b
Inzira <inzira1995@gmail.com>
2025-08-04 14:41:14 -0700

    Added skills component

--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,5 +1,6 @@
 import "./Main.css";
 import About from "../About/About";
+import Skills from "../Skills/Skills";
 
 export default function Main() {
   return (
@@ -7,6 +8,7 @@ export default function Main() {
       <hi className="main__title">Hello, I'm Inzira Abdurafikova</hi>
       <h2 className="main__position">Software engineer</h2>
       <About />
+      <Skills />
     </div>
   );
 }
---
+++ src/components/Skills/Skills.jsx
@@ -0,0 +1,18 @@
+import "./Skills.css";
+
+export default function Skills() {
+  return (
+    <div className="skills">
+      Skills
+      <ul className="skills__content">
+        <li>
+          Frontend: HTML, CSS, JavaScript, TypeScript, React, Tailwind, Figma,
+          Vite
+        </li>
+        <li>Backend: Node.js, Express, MongoDB, PostgreSQL, Mongoose</li>
+        <li> Tools: Git/GitHub, Postman, REST APIs, Jest</li>
+        <li>Other: Agile methodologies, Data Structures & Algorithms, OOP</li>
+      </ul>
+    </div>
+  );
+}
