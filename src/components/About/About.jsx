fd9f388b132de8c2a37141a68b47b33f512ebe51
Inzira <inzira1995@gmail.com>
2025-08-07 23:42:10 -0700

    worked on main, about and skills sections

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,18 +1,19 @@
 import "./About.css";
-import Avatar from "../../assets/Avatar.png";
 
 export default function About() {
   return (
     <section className="about">
       <div className="about__content">
-        My journey into software development started with a love for creating
-        things and a curiosity for how technology works.After years in sales, I
-        decided to follow my passion for problem-solving and began learning web
-        development. Now, as a full stack developer, I combine technical skills
-        with creativity to craft user-focused digital experiences. Every project
-        is a new opportunity to learn, adapt, and grow.
+        <h2 className="about__title">About me</h2>
+        <p className="about__text">
+          My journey into software development started with a love for creating
+          things and a curiosity for how technology works.After years in sales,
+          I decided to follow my passion for problem-solving and began learning
+          web development. Now, as a full stack developer, I combine technical
+          skills with creativity to craft user-focused digital experiences.
+          Every project is a new opportunity to learn, adapt, and grow.
+        </p>
       </div>
-      <img src={Avatar} alt="Avatar" className="about__image" />
     </section>
   );
 }
--- src/components/Main/Main.jsx
+++ src/components/Main/Main.jsx
@@ -1,14 +1,20 @@
 import "./Main.css";
 import About from "../About/About";
 import Skills from "../Skills/Skills";
+import Avatar from "../../assets/Avatar.png";
 
 export default function Main() {
   return (
-    <div className="main">
-      <hi className="main__title">Hello, I'm Inzira Abdurafikova</hi>
-      <h2 className="main__position">Software engineer</h2>
+    <section className="main">
+      <div className="main__content">
+        <div className="main__text">
+          <hi className="main__title">Hello, I'm Inzira Abdurafikova</hi>
+          <h2 className="main__position">Software engineer</h2>
+        </div>
+        <img src={Avatar} alt="Avatar" className="main__image" />
+      </div>
       <About />
       <Skills />
-    </div>
+    </section>
   );
 }
