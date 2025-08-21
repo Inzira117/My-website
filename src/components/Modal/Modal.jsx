7dc455b08aaf4e5cd996106baf66ecf6b4ab054e
Inzira <inzira1995@gmail.com>
2025-08-21 13:29:27 -0700

    Created mod al function

---
+++ src/components/Modal/Modal.jsx
@@ -0,0 +1,11 @@
+import "./Modal.css";
+
+export default function Modal() {
+  return (
+    <div className="modal">
+      Contact me
+      <p className="modal__content">email: inzira1995@gmail.com</p>
+      <p className="modal__content">phone: +1(323) 536 69018</p>
+    </div>
+  );
+}
