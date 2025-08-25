412c2456b20ebbe1e1d0772f21fc07e3db94a2f0
Inzira <inzira1995@gmail.com>
2025-08-25 13:03:49 -0700

    Added media queries

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,12 +1,19 @@
 import "./Header.css";
+import Menu from "../../assets/menu.png";
+import { useState } from "react";
 
 export default function Header({ openModal }) {
+  const [isOpen, setIsOpen] = useState(false);
+
   return (
     <header className="header">
       <a href="/" className="header__logo">
         Inzira A.
       </a>
-      <nav className="header__nav">
+      <div className="header__hamburger" onClick={() => setIsOpen(!isOpen)}>
+        <img src={Menu} alt="menu" />
+      </div>
+      <nav className={`nav ${isOpen ? "open" : ""}`}>
         <a href="#about" className="header__nav-link">
           About
         </a>
