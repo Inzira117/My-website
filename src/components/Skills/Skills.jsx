18b93f4282b3e188e6a2d6fe29c25eaa1ff29d52
Inzira <inzira1995@gmail.com>
2025-10-15 20:20:11 -0700

    working with animations in Skills section

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,3 +1,4 @@
+import { motion } from "framer-motion";
 import "./Skills.css";
 import ReactImg from "../../assets/react.svg";
 import HtmlImg from "../../assets/html.png";
@@ -5,14 +6,29 @@ import CssImg from "../../assets/css.png";
 import JsImg from "../../assets/js.png";
 
 export default function Skills() {
+  const images = [
+    { src: HtmlImg, alt: "HTML" },
+    { src: CssImg, alt: "CSS" },
+    { src: JsImg, alt: "JavaScript" },
+    { src: ReactImg, alt: "React" },
+  ];
+
   return (
     <section className="skills" id="skills">
       <h2 className="skills__title">Skills</h2>
       <div className="skills__images">
-        <img src={ReactImg} alt="React" className="skills__image" />
-        <img src={HtmlImg} alt="HTML" className="skills__image" />
-        <img src={CssImg} alt="CSS" className="skills__image" />
-        <img src={JsImg} alt="JavaScript" className="skills__image" />
+        {images.map((img, index) => (
+          <motion.img
+            key={index}
+            src={img.src}
+            alt={img.alt}
+            className="skills__image"
+            initial={{ opacity: 0, y: 30 }}
+            animate={{ opacity: 1, y: 0 }}
+            transition={{ delay: index * 0.2, duration: 0.8 }}
+            whileHover={{ scale: 1.1, rotate: 5 }}
+          />
+        ))}
       </div>
       <ul className="skills__content">
         <li>
