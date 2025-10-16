a76f3ddd362ed85aaefbee872c201ac94ae728d2
Inzira <inzira1995@gmail.com>
2025-10-15 20:17:23 -0700

    Adding animations

--- src/components/Skills/Skills.jsx
+++ src/components/Skills/Skills.jsx
@@ -1,9 +1,19 @@
 import "./Skills.css";
+import ReactImg from "../../assets/react.svg";
+import HtmlImg from "../../assets/html.png";
+import CssImg from "../../assets/css.png";
+import JsImg from "../../assets/js.png";
 
 export default function Skills() {
   return (
     <section className="skills" id="skills">
       <h2 className="skills__title">Skills</h2>
+      <div className="skills__images">
+        <img src={ReactImg} alt="React" className="skills__image" />
+        <img src={HtmlImg} alt="HTML" className="skills__image" />
+        <img src={CssImg} alt="CSS" className="skills__image" />
+        <img src={JsImg} alt="JavaScript" className="skills__image" />
+      </div>
       <ul className="skills__content">
         <li>
           Frontend: HTML, CSS, JavaScript, TypeScript, React, Tailwind, Figma,
